cae82e6fa8b4a26574718713d9a4dffa0324d363
Tomasz Murjas <w41332@student.wsiz.edu.pl>
2024-10-24 11:21:40 +0200

    moje

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,9 +4,68 @@
     {
         static void Main(string[] args)
         {
-            var abc= 10;
-            Console.WriteLine($"Hello, World {abc}");
-            //AAAAAAAAAABBBBb
+
+
+
+            void LiczbaParzysta()
+            {
+            int n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            { Console.WriteLine("tak"); }
+            else
+            { Console.WriteLine("nie"); }
+            }
+
+
+            void WriteEvent()
+            { 
+            Console.WriteLine("podaj n");
+            var n = Console.ReadLine();
+            //var abc= 10;
+            //Console.WriteLine($"Hello, World {abc}");
+             for (var i = 0; i < int.Parse(n); i += 2)
+             {
+                 Console.WriteLine(i);
+             }
+            }
+
+            int MojaSilnia(int x)
+            {
+                if (x == 0) return 1;
+                return MojaSilnia(x - 1);
+            }
+
+
+            while(true)
+            {
+                Console.WriteLine("wybierz");
+                Console.WriteLine("1. liczba parzsta");
+                Console.WriteLine("2. wypisz liczby");
+                Console.WriteLine("3. silnia");
+                Console.WriteLine("4. gra");
+                Console.WriteLine("5.wyjscie");
+                int opcja = int.Parse(Console.ReadLine());
+
+                switch (opcja)
+                {
+                    case 1: LiczbaParzysta(); break;
+                    case 2: WriteEvent(); break;
+                    case 3: break;
+                    default: break;
+
+                }
+
+                if (opcja == 5)
+                    break;
+
+
+
+
+            }
+
+
+
+          
         }
     }
 }
